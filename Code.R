61ced3ae0ea58c7f756524887dfe6aff88fa55e3
LYH-2021 <yuhui.liu.u@gmail.com>
2022-11-15 18:41:35 +0100

    new code and data.

--- Code.R
+++ Code.R
@@ -3,11 +3,9 @@ library(dplyr)
 library(magrittr)
 
 load('looking.RData')
-load('touch.RData')
 load('fNIRSData.RData')
 load('age_sex.RData')
-load('demografics.RData')
-load('contingency.RData')
+
 
 fNIRS2 <- pivot_wider(data = fNIRSData,
                       names_from = c(condition, channel),
@@ -15,10 +13,6 @@ fNIRS2 <- pivot_wider(data = fNIRSData,
 looking2 <- pivot_wider(data = looking, 
                         names_from = c(trial, view), 
                         values_from = duration)
-touch2 <- pivot_wider(data = touch, names_from = category, values_from = n_rel)
-data.list1 <- list(fNIRS2, looking2, touch2)
-touch.looking.fNIRS <- data.list1 %>%
-  reduce(full_join, by = "id")
-data.list2 <- c(data.list1, list(age_sex, demografics, contingency))
-total.data <- data.list2 %>% 
+data.list <- list(fNIRS2, looking2, age_sex)
+fNIRS.looking.age_sex <- data.list1 %>%
   reduce(full_join, by = "id")
